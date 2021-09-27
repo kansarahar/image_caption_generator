5937e1e21a5dc7bbcfd6152a105e99ed10cf718a
Haresh Kansara <kansarahar@gmail.com>
2021-09-27 02:39:04 -0400

    cleaned up the data a bit more

--- parse_captions.py
+++ parse_captions.py
@@ -36,39 +36,91 @@ bad_captions = []
 for num,row in enumerate(captions_content):
   image_name = row[:row.find('#')].strip().lower()
   image_caption = row[row.find('#')+2:].strip().lower()
-  # if no period at the end of the sentence, add one
-  image_caption = image_caption if image_caption[-1] == '.' else '%s .' % image_caption
-  # all @'s are just mistyped 2's
-  image_caption = image_caption.replace('@', '2')
-  # replace any unnecessary '!' with '.'
-  image_caption = image_caption.replace('!', '.') if image_caption.find('!') == len(image_caption)-1 else image_caption
-  # remove quotes 
-  image_caption = re.sub(r' (saying|that says|that says ,|that reads|that reads ,|reading|named|labeled|called|called the|marked|that states|that states ,) ("(.*)"|\'(.*)\')', '', image_caption)
-  image_caption = re.sub(r' (a|reads|that read) ("(.*)"|\'(.*)\')', '', image_caption)
 
+  #correct punctuation
+  image_caption = image_caption.replace('!', '.') if image_caption.find('!') == len(image_caption)-1 else image_caption
+  image_caption = image_caption.replace('?', '.') if image_caption.find('?') == len(image_caption)-1 else image_caption
+  image_caption = image_caption if image_caption[-1] == '.' else '%s .' % image_caption
+  
+  # specific caption fixes found
+  image_caption = image_caption.replace('@', '2')
+  image_caption = image_caption.replace('&', 'and')
+  image_caption = image_caption.replace('? ', '')
+  image_caption = image_caption.replace('etc.', 'etc')
+  image_caption = image_caption.replace('g ?uys', 'guys')
+  image_caption = image_caption.replace(' : ', ' ; ')
+  image_caption = image_caption.replace('wuth', 'with')
+  image_caption = image_caption.replace(':while', 'while')
+  image_caption = image_caption.replace('the name', 'a name')
+  image_caption = image_caption.replace('" canal "', 'canal')
+  image_caption = image_caption.replace('\' devil \'', 'devil')
+  image_caption = image_caption.replace('a " bath "', 'a bath')
+  image_caption = image_caption.replace('" boombox "', 'boombox')
+  image_caption = image_caption.replace('" dance cage "', 'cage')
+  image_caption = image_caption.replace('s gray dog', 'a gray dog')
+  image_caption = image_caption.replace('" high-five "', 'high-five')
+  image_caption = image_caption.replace('" farmers market "', 'market')
+  image_caption = image_caption.replace('" beer gardens "', 'beer garden')
+  image_caption = image_caption.replace('the " new testament "', 'a book')
+  image_caption = image_caption.replace('an " angry " man " chokes "', 'a man')
+  image_caption = image_caption.replace('" city picnic . "', 'city picnic')
+  image_caption = image_caption.replace('professed " naked cowboy "', 'naked cowboy')
+  image_caption = image_caption.replace('" moon bounce , "', 'moon bounce')
+  image_caption = image_caption.replace('passed a \' model express \'', 'past a shop')
+  image_caption = image_caption.replace('" hartman prehistoric garden "', 'garden')
+  image_caption = image_caption.replace('hartman prehistoric garden', 'garden')
+  image_caption = image_caption.replace('hartman prehistoric center', 'garden center')
+  image_caption = image_caption.replace('prehistoric garden', 'garden')
+  image_caption = image_caption.replace('i do n\'t see a picture i do n\'t see a picture i do n\'t see a picture .', 'a woman holds her head as she is making a phone call .')
+  image_caption = image_caption.replace('i do n\'t see a picture i do n\'t see a picture i do n\'t see a picture i do n\'t see a picture .', 'three men sitting on stair steps talking to each other .')
+
+
+  image_caption = image_caption.replace('a " telephone booth "', 'a telephone booth')
+  image_caption = image_caption.replace('points to a " one " outside', 'points outside')
+  image_caption = image_caption.replace('man with a " backyard babies " has', 'man has')
+  image_caption = image_caption.replace('experiencing and exhibit', 'experiencing an exhibit')
+  image_caption = image_caption.replace('two you women strolling', 'two young women strolling')
+  image_caption = image_caption.replace('a " rock star " performing', 'a performer performing')
+  image_caption = image_caption.replace('banner that stays , " we are demanding . " .', 'banner .')
+  image_caption = image_caption.replace('republicans have a " i stole all your money hahaha " outdoor barbecue .', 'people have an outdoor barbecue .')
+  image_caption = image_caption.replace('with a " do not tread on me " flag and a sign that says " Thank you U.S. Troops . "', 'with a flag and a sign .')
+  image_caption = image_caption.replace('other guy the number " o " as this what they do for a living .', 'other guy .')
+  
+  # remove quotes 
+  image_caption = re.sub(r' (after saying ,|that say ,|that says ,|which reads ,|that reads ,|that read ,|that states ,|with the phrase ,) ("(.*)"|\'(.*)\')', '', image_caption)
+  image_caption = re.sub(r' (saying ,|that says|that reads|reading|marked|that states|named|called the|that is labeled|labeled a|of which say|that all say|with the phrase|entitled) ("(.*)"|\'(.*)\')', '', image_caption)
+  image_caption = re.sub(r' (saying|that say|labeled|called|reads|that read|which say|titled) ("(.*)"|\'(.*)\')', '', image_caption)
+  image_caption = re.sub(r' (with the text|displaying the text|that contains the text|called|reads|that read|which say|which states|which reads|that reads :|reading :) ("(.*)"|\'(.*)\')', '', image_caption)
+
+  image_caption = re.sub(r'a ("(.*)"|\'(.*)\')', ' a', image_caption)
+
+  image_caption = re.sub(r'the words ("(.*)"|\'(.*)\')', 'words', image_caption)
+  image_caption = re.sub(r'the word ("(.*)"|\'(.*)\')', 'words', image_caption)
+  image_caption = re.sub(r'(displays|says) ("(.*)"|\'(.*)\')', 'says something', image_caption)
   image_caption = re.sub(r'(a|an) "(.*)" sign', 'a sign', image_caption)
   image_caption = re.sub(r'with "(.*)" sign', 'with some sign', image_caption)
-  image_caption = re.sub(r'the words "(.*)"', 'words', image_caption)
+  image_caption = re.sub(r'with "(.*)"', 'with something', image_caption)
+  image_caption = re.sub(r'"(.*)" written', 'something written', image_caption)
+  image_caption = re.sub(r' (an ollie|an " ollie "|a ollie)', ' a trick', image_caption)
+  image_caption = re.sub(r' ollies', ' does a trick', image_caption)
+  image_caption = re.sub(r'prehistoric garden|hartman prehistoric garden|hartman prehistoric', 'garden', image_caption)
+  image_caption = re.sub(r' (\d\d % off|\d\d %)', '', image_caption)
 
-  #remove double spaces
-  image_caption = re.sub(r'  ', ' ', image_caption)
+  image_caption = re.sub(r'" (.*) "', '', image_caption)
+  image_caption = re.sub(r'\'(?![sm(re)])(.*)\'(?![sm(re)]) ', '', image_caption)
+  image_caption = re.sub(r' \' |(?<=\w)\' ', ' ', image_caption)
+  image_caption = re.sub(r'\((.*)\)', '', image_caption)
+
+
+  # if no period at the end of the sentence, add one
+  image_caption = image_caption.strip()
+  image_caption = image_caption if image_caption[-1] == '.' else '%s .' % image_caption
+
+  # remove double spaces
+  image_caption = re.sub(r'  ', ' ', image_caption).strip()
 
   print(image_name[-8:], image_caption)
-  bad_condition = '.' in image_caption and image_caption.find('.') < len(image_caption)-1
-  bad_condition = bad_condition or '"' in image_caption
-  bad_condition = bad_condition or ('\'' in image_caption and not '\'s' in image_caption)
-  if bad_condition:
-    bad_captions.append(image_caption)
 
-    r"'(?![sm(re)])(.*)'(?![sm(re)])"
-  # if ('!' in image_caption and '"' not in image_caption):
-  #   print(image_caption)
-  # naughty_chars = '~`!@$%^*()'
-  # if any([char in image_caption for char in naughty_chars]) and not bad_condition:
-
-  #   print(image_caption)
-
-# print(len(bad_captions))
 
 ###################### </ parse captions file > #######################
 
